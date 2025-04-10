05a2ace746ca9e942777c37e9d5b3ab5bddb1333
draff1800 <draff1800@gmail.com>
2025-04-10 17:03:37 -0400

    Returning Bad Request if username or email is duplicated

--- src/controllers/auth-controller.ts
+++ src/controllers/auth-controller.ts
@@ -3,26 +3,35 @@ import { User } from '../db/models/user-model.js';
 import { logger } from '../utils/logger.js';
 import { InternalServerError } from '../errors/custom-errors/internal-server-error.js';
 import type { Request, Response, NextFunction } from 'express';
+import { BadRequestError } from '../errors/custom-errors/bad-request-error.js';
+import mongoose from 'mongoose';
 
 const register = async (username: string, email: string, password: string) => {
-  try {
-    const passwordHash = await bcrypt.hash(password, 10);
+  const passwordHash = await bcrypt.hash(password, 10);
 
-    const savedUser = await User.create({
+  let savedUser;
+
+  try {
+    savedUser = await User.create({
       username,
       email,
       passwordHash,
     });
-
-    logger.info('New user saved', {
-      userId: savedUser._id,
-      username: savedUser.username,
-    });
-
-    return savedUser.serialize();
-  } catch {
-    throw new InternalServerError("Couldn't register user. Please try again later");
+  } catch (err: unknown) {
+    if (err instanceof mongoose.mongo.MongoServerError && err.code === 11000) {
+      const field = Object.keys(err.keyValue)[0];
+      throw new BadRequestError(`${field} is already taken`);
+    } else {
+      throw new InternalServerError(`Couldn't register user. Please try again later`);
+    }
   }
+
+  logger.info('New user saved', {
+    userId: savedUser._id,
+    username: savedUser.username,
+  });
+
+  return savedUser.serialize();
 };
 
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
