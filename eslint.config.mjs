f305ca0899534912573c93dfb1f27af138313fb7
draff1800 <draff1800@gmail.com>
2025-04-18 01:51:18 -0400

    Added import ordering, dev/build tsconfigs, and new scripts

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -3,6 +3,7 @@
 import eslint from '@eslint/js';
 import tseslint from 'typescript-eslint';
 import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended';
+import simpleImportSort from 'eslint-plugin-simple-import-sort';
 
 export default tseslint.config(
   eslint.configs.recommended,
@@ -10,7 +11,13 @@ export default tseslint.config(
   tseslint.configs.stylistic,
   eslintPluginPrettierRecommended,
   {
+    plugins: {
+      'simple-import-sort': simpleImportSort,
+    },
     rules: {
+      'simple-import-sort/imports': 'error',
+      'simple-import-sort/exports': 'error',
+
       'no-unused-vars': 'off',
       '@typescript-eslint/no-unused-vars': [
         'error',
